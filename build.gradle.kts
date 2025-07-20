9b1055dcfad95c7fb088a4f2ab4f8b6bd4b460f2
Artur Nadreev <nadarth@yandex.ru>
2025-07-20 21:28:19 +0300

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java")
+}
+
+group = "io.synergy"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
